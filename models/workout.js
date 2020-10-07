c6c9656ee1fb2ff40c3d8dfc44644df62e397ae6
Brendon Conatser <b.conatser21@gmail.com>
2020-10-07 16:04:32 -0500

    created the workout model

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutschema = new Schema({
-
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of exercise!"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for the exercise!"
+      },
+      duration: {
+        type: Number,
+        required: "Enter your time in number of minutes!"
+      },
+      sets: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    }
+  ]
 });
 
 const Workout = mongoose.model("Workout", workoutschema);
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useFindAndModify: false });
 
 app.listen(PORT, () => {
   console.log("Server listening on: http://localhost:" + PORT);
