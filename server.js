08dec6626c4d47aeb76c21bc2bba9b81e9816b75
Brendon Conatser <b.conatser21@gmail.com>
2020-10-08 14:51:33 -0500

    fixed problem with heroku

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use(htmlRoutes);
 app.use(apiRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log(`Server listening on: http://localhost: ${PORT}`);
 });
